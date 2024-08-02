491e00fdd0c7e67928ce0ef49c4bc0faf5509c51
micahkepe <micahkepe@gmail.com>
2024-08-02 15:00:11 -0700

    Initial commit

---
+++ vimtutor-sequel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the location of the vimtutor-sequel.txt file and vimrc
+TUTORIAL_FILE="/usr/local/share/vimtutor-sequel/vimtutor-sequel.txt"
+VIMRC_FILE="/usr/local/share/vimtutor-sequel/vimtutor-sequel.vimrc"
+TEMP_FILE="/tmp/vimtutor-sequel.txt"
+
+# Check if the tutorial file exists
+if [ ! -f "$TUTORIAL_FILE" ]; then
+  echo "Vimtutor Sequel file not found!"
+  exit 1
+fi
+
+# Ensure the temporary file is deleted if it already exists
+rm -f "$TEMP_FILE"
+
+# Copy the tutorial file to a temporary location
+cp "$TUTORIAL_FILE" "$TEMP_FILE"
+
+# Open the temporary tutorial file in Vim with the custom vimrc
+vim -u "$VIMRC_FILE" "$TEMP_FILE"
+
+# Clean up the temporary file after Vim exits
+rm -f "$TEMP_FILE"
+
---
+++ vimtutor-sequel.vimrc
@@ -0,0 +1,11 @@
+" Make Vim configuration try to replicate original `vimtutor`
+set nocompatible
+filetype off
+syntax off
+set nolinebreak
+set nowrap
+set nonumber
+set norelativenumber
+set background=dark
+highlight Normal ctermfg=grey ctermbg=black
+
